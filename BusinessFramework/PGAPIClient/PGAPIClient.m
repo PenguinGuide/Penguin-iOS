0f326b7e2c2197120338f9f0a95c950ecd346d69
Kobe Dai <kobe.dai@penguinguide.com>
2016-06-27 17:35:06 +0800

    add business framework and base framework

---
+++ BusinessFramework/PGAPIClient/PGAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  PGAPIClient.m
+//  Penguin
+//
+//  Created by Jing Dai on 6/27/16.
+//  Copyright © 2016 Xinglian. All rights reserved.
+//
+
+#import "PGAPIClient.h"
+
+@implementation PGAPIClient
+
+@end
