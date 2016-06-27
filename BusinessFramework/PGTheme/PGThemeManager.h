3cc1f1dd9b2d74e181c273241a64d94fb1ef7e3f
Kobe Dai <kobe.dai@penguinguide.com>
2016-06-27 20:55:21 +0800

    share manager theme manager

---
+++ BusinessFramework/PGTheme/PGThemeManager.h
@@ -0,0 +1,13 @@
+//
+//  PGTheme.h
+//  Penguin
+//
+//  Created by Jing Dai on 6/27/16.
+//  Copyright © 2016 Xinglian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGThemeManager : NSObject
+
+@end
